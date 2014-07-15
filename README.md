c4e70eea9d017e9114fb4a360403686f2d58b8ed
Changbeom Ahn <kewlbear@gmail.com>
2014-07-15 14:36:38 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-fdk-aac-build-script-for-iOS
-============================
+# fdk-aac-build-script-for-iOS
+
+Shell script to build fdk-aac for use in iOS apps.
+
+## Usage
+
+* Build all:
+
+```
+build-fdk-aac.sh
+```
+
+* Build for some architectures:
+
+```
+build-fdk-aac.sh armv7s x86_64
+```
+
+* Build universal library from separately built architectures:
+
+```
+build-fdk-aac.sh lipo
+```
+
+## Caveat
+
+No arm64 support yet
---
+++ build-fdk-aac.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+CONFIGURE_FLAGS="--enable-static --with-pic=yes --disable-shared"
+
+ARCHS="armv7s x86_64 i386 armv7"
+
+# directories
+SOURCE="fdk-aac-0.1.3"
+FAT="fdk-aac-ios"
+
+SCRATCH="scratch"
+# must be an absolute path
+THIN=`pwd`/"thin"
+
+COMPILE="y"
+LIPO="y"
+
+if [ "$*" ]
+then
+	if [ "$*" = "lipo" ]
+	then
+		# skip compile
+		COMPILE=
+	else
+		ARCHS="$*"
+		if [ $# -eq 1 ]
+		then
+			# skip lipo
+			LIPO=
+		fi
+	fi
+fi
+
+if [ "$COMPILE" ]
+then
+	CWD=`pwd`
+	for ARCH in $ARCHS
+	do
+		echo "building $ARCH..."
+		mkdir -p "$SCRATCH/$ARCH"
+		cd "$SCRATCH/$ARCH"
+
+		CFLAGS="-arch $ARCH"
+
+		if [ "$ARCH" = "i386" -o "$ARCH" = "x86_64" ]
+		then
+		    PLATFORM="iPhoneSimulator"
+		    CPU=
+		    if [ "$ARCH" = "x86_64" ]
+		    then
+		    	CFLAGS="$CFLAGS -mios-simulator-version-min=7.0"
+		    	HOST=
+		    else
+		    	CFLAGS="$CFLAGS -mios-simulator-version-min=7.0"
+			HOST="--host=i386-apple-darwin"
+		    fi
+		else
+		    PLATFORM="iPhoneOS"
+		    if [ $ARCH = arm64 ]
+		    then
+		        #CFLAGS="$CFLAGS -D__arm__ -D__ARM_ARCH_7EM__" # hack!
+		        HOST="--host=aarch64-apple-darwin"
+                    else
+		        HOST="--host=arm-apple-darwin"
+	            fi
+		    SIMULATOR=
+		fi
+
+		XCRUN_SDK=`echo $PLATFORM | tr '[:upper:]' '[:lower:]'`
+		CC="xcrun -sdk $XCRUN_SDK clang -Wno-error=unused-command-line-argument-hard-error-in-future"
+		AS="$CWD/$SOURCE/extras/gas-preprocessor.pl $CC"
+		CXXFLAGS="$CFLAGS"
+		LDFLAGS="$CFLAGS"
+
+		$CWD/$SOURCE/configure \
+		    $CONFIGURE_FLAGS \
+		    $HOST \
+		    $CPU \
+		    CC="$CC" \
+		    CXX="$CC" \
+		    CPP="$CC -E" \
+                    AS="$AS" \
+		    CFLAGS="$CFLAGS" \
+		    LDFLAGS="$LDFLAGS" \
+		    CPPFLAGS="$CFLAGS" \
+		    --prefix="$THIN/$ARCH"
+
+		make -j3 install
+		cd $CWD
+	done
+fi
+
+if [ "$LIPO" ]
+then
+	echo "building fat binaries..."
+	mkdir -p $FAT/lib
+	set - $ARCHS
+	CWD=`pwd`
+	cd $THIN/$1/lib
+	for LIB in *.a
+	do
+		cd $CWD
+		lipo -create `find $THIN -name $LIB` -output $FAT/lib/$LIB
+	done
+
+	cd $CWD
+	cp -rf $THIN/$1/include $FAT
+fi
